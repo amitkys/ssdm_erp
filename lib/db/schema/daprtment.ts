a20ef9b9cd0ede3af3d4126f7e07da1341703f38
Danish <danishshamshee123@gmail.com>
2026-05-20 00:07:34 +0530

    test

--- lib/db/schema/daprtment.ts
+++ lib/db/schema/daprtment.ts
@@ -123,3 +123,6 @@ export const feeTable = pgTable('fee', {
     createdAt: timestamp().defaultNow().notNull(),
     updatedAt: timestamp().defaultNow().notNull(),
 });
+
+
+// Hello Kys please igniore this message
