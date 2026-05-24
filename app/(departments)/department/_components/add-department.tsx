91c85eabb70d6db5ac28a13c8693c0ad65828ed4
Danish <danishshamshee123@gmail.com>
2026-05-25 01:37:32 +0530

    show department details on card and add new department UI is completed

---
+++ app/(departments)/department/_components/add-department.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Field, FieldGroup } from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { useForm } from "react-hook-form"
+import { type NewDepartmentType, newDepartmentSchema } from "../lib/zod-type/new-department-type"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { InputForDepartment } from "./input-for-department"
+
+export function AddDepartment() {
+
+    const form = useForm<NewDepartmentType>({
+        resolver: zodResolver(newDepartmentSchema),
+        defaultValues: {
+            name: "",
+            code: "",
+            description: ""
+        }
+    })
+
+    const onSubmit = (data: NewDepartmentType)=>{
+        console.log(data)
+    }
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant={"secondary"} className="text-blue-900 font-bold cursor-pointer shadow-lg">New Department</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-sm">
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <DialogHeader>
+            <DialogTitle className="font-semibold">Add New Department</DialogTitle>
+            <DialogDescription>
+              Add a new department here. Click save when you&apos;re
+              done.
+            </DialogDescription>
+          </DialogHeader>
+          <div className="flex flex-col items-center justify-center gap-5">
+            <InputForDepartment form={form}/>
+          </div>
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button variant="outline">Cancel</Button>
+            </DialogClose>
+            <Button type="submit">Save changes</Button>
+          </DialogFooter>
+        </form>
+      </DialogContent>
+    </Dialog>
+  )
+}
--- app/(departments)/department/_components/list-department.tsx
+++ app/(departments)/department/_components/list-department.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useQuery } from "@tanstack/react-query";
 import { getDepartment } from "../query/get-all-department";
+import { DetailsCard } from "./details-carad";
 
 export function ListDepartment() {
   const { data, isLoading, error } = useQuery(getDepartment());
@@ -17,10 +18,11 @@ export function ListDepartment() {
   }
   return (
     <>
-      <h1>Department List</h1>
-      <ul>
+      {/* <h1>Department List</h1> */}
+      <ul className="grid grid-cols-3 gap-2">
         {data.map((item) => (
-          <li key={item.id}>{item.name}</li>
+          // <li key={item.id}>{item.name}</li>
+          <DetailsCard key={item.id} id={item.id} name={item.name} code={item.code} description={item.description} />
         ))}
       </ul>
     </>
