1511a3295042cea76a8006db33a3fbcde8bf5463
Danish Shamshee <danishshamshee123@gmail.com>
2026-08-12 00:55:56 +0530

    create a feature to show the faculties information on home screen

---
+++ lib/faculty-data.ts
@@ -0,0 +1,195 @@
+
+
+export interface Faculty{
+  id: number;
+  name: string;
+  department: string;
+  designation: string;
+  email: string | null;
+  phone: string | null;
+  avatar: string | null;
+  status: "ACTIVE" | "INACTIVE";
+}
+
+export const faculties: Faculty[] = [
+ {
+    "id": 1,
+    "name": "Dr. Kaushal Kishore Singh",
+    "department": "Department of Chemistry",
+    "designation": "Prof. Incharge",
+    "email": "kksingh754929@gmail.com",
+    "phone": "7549298333",
+    "avatar": "faculty1.jpg",
+    "status": "ACTIVE"
+  },
+  {
+    "id": 2,
+    "name": "Sushama Kumari",
+    "department": "Department of Home Science",
+    "designation": "Assistant Professor & Proctorial",
+    "email": "Sinhahouse16@gmail.com",
+    "phone": "9798447636",
+    "avatar": "faculty2.jpg",
+    "status": "ACTIVE"
+  },
+  {
+    "id": 3,
+    "name": "Sanjay Kumar Singh",
+    "department": "Department of Physics",
+    "designation": "Assistant Professor",
+    "email": "SanjaySingh14011968@gmail.com",
+    "phone": "6205427066",
+    "avatar": "faculty3.jpg",
+    "status": "ACTIVE"
+  },
+  {
+    "id": 4,
+    "name": "Arun Prasad",
+    "department": "Department of Physics",
+    "designation": "Assistant Professor",
+    "email": "ap2707232@gmail.com",
+    "phone": "9572363726",
+    "avatar": "faculty4.jpg",
+    "status": "ACTIVE"
+  },
+  {
+    "id": 5,
+    "name": "Dr. Madhu Ranjan",
+    "department": "Department of Economics",
+    "designation": "Assistant Professor",
+    "email": "ranjan.madhu18@gmail.com",
+    "phone": "7903588743",
+    "avatar": "faculty5.jpg",
+    "status": "ACTIVE"
+  },
+  {
+    "id": 6,
+    "name": "Dr. Rajesh Kumar Verma",
+    "department": "Department of Mathematics",
+    "designation": "Assistant Professor S.S.D.M.C.",
+    "email": "rkumarverma007@gmail.com",
+    "phone": "6200085327",
+    "avatar": "faculty6.jpg",
+    "status": "ACTIVE"
+  },
+  {
+    "id": 7,
+    "name": "Prawin Kumar Singh",
+    "department": "Department of Botany",
+    "designation": "Assistant Professor",
+    "email": "prawinkumarsingh8396@gmail.com",
+    "phone": "9631866561",
+    "avatar": "faculty7.jpg",
+    "status": "ACTIVE"
+  },
+  {
+    "id": 8,
+    "name": "Kumari Aruna",
+    "department": "Department of Psychology",
+    "designation": "Assistant Professor",
+    "email": "Aruna31163@gmail.com",
+    "phone": "7870664176",
+    "avatar": "faculty8.jpg",
+    "status": "ACTIVE"
+  },
+  {
+    "id": 9,
+    "name": "Sanjay Kumar Singh",
+    "department": "Department of History",
+    "designation": "Asst. Professor",
+    "email": "ssanjay84389@gmail.com",
+    "phone": "9934981835",
+    "avatar": "faculty9.jpg",
+    "status": "ACTIVE"
+  },
+  {
+    "id": 10,
+    "name": "Dr. Shree Hari Kesari",
+    "department": "Department of Pol. Sc.",
+    "designation": "Assistant Professor",
+    "email": "drshreeharikesari@gmail.com",
+    "phone": "9835474573",
+    "avatar": "faculty10.jpg",
+    "status": "ACTIVE"
+  },
+  {
+    "id": 11,
+    "name": "Om Prakash Singh",
+    "department": "Department of Chemistry",
+    "designation": "Assistant Professor",
+    "email": "omprakashsingh792440@gmail.com",
+    "phone": "9693085283",
+    "avatar": "faculty11.jpg",
+    "status": "ACTIVE"
+  },
+  {
+    "id": 12,
+    "name": "Nirmal Kumar Singh",
+    "department": "Department of Political Science",
+    "designation": "Assistant Professor",
+    "email": "nirmalchunchun@gmail.com",
+    "phone": "7782083662",
+    "avatar": "faculty12.jpg",
+    "status": "ACTIVE"
+  },
+  {
+    "id": 13,
+    "name": "Suprita Kumari",
+    "department": "Department of Philosophy",
+    "designation": "Assistant Professor",
+    "email": "Singhsuprita67@gmail.com",
+    "phone": "9234021453",
+    "avatar": "faculty13.jpg",
+    "status": "ACTIVE"
+  },
+  {
+    "id": 14,
+    "name": "Arvind Kumar Singh",
+    "department": "Department of Public Administration",
+    "designation": "Assistant Professor",
+    "email": "arvindkumarsinghbarh99340@gmail.com",
+    "phone": "8969462164",
+    "avatar": "faculty14.jpg",
+    "status": "ACTIVE"
+  },
+  {
+    "id": 15,
+    "name": "Randhir Kumar Singh",
+    "department": "Department of Ancient History (A.I.A.S)",
+    "designation": "Asst. Professor",
+    "email": "snrandhir2@gmail.com",
+    "phone": "8789472653",
+    "avatar": "faculty15.jpg",
+    "status": "ACTIVE"
+  },
+  {
+    "id": 16,
+    "name": "Jitendra Pd. Singh",
+    "department": "Department of L.S.W.",
+    "designation": "Assistant Professor",
+    "email": "Jitendra.Prasad.singh1960@gmail.com",
+    "phone": "9122944555",
+    "avatar": "faculty16.jpg",
+    "status": "ACTIVE"
+  },
+  {
+    "id": 17,
+    "name": "Dr. Thakur Randhir Singh",
+    "department": "Department of Sociology",
+    "designation": "Asst. Professor",
+    "email": "Thakur.randhir.singh.55@gmail.com",
+    "phone": "9199081076",
+    "avatar": "faculty17.jpg",
+    "status": "ACTIVE"
+  },
+  {
+    "id": 18,
+    "name": "Umesh Prasad",
+    "department": "Department of L.S.W.",
+    "designation": "Asst. Professor",
+    "email": "umeshprasad98010@gmail.com",
+    "phone": "8210251988",
+    "avatar": "faculty18.jpg",
+    "status": "ACTIVE"
+  }
+]
